aaf8804e2ed92d6dd722c9837e6cea448974cfb9
Martin Grames <mgrames@linuxbox.cz>
2018-06-26 13:16:32 +0200

    Fixed indentation.

--- rest-api.php
+++ rest-api.php
@@ -4,25 +4,25 @@ define('CERT', './custpbx-demo1.pem');
 
 function api_call($action, $data) {
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, URL . $action);
-    curl_setopt($ch, CURLOPT_POST, TRUE);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, URL . $action);
+	curl_setopt($ch, CURLOPT_POST, TRUE);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_SSLCERT, CERT);
+	curl_setopt($ch, CURLOPT_SSLCERT, CERT);
 
-    $response = curl_exec($ch);
-    if ($response === false) {
-        echo 'Connection error: ' . curl_error($ch) . "\n";
-        return null;
-    }
+	$response = curl_exec($ch);
+	if ($response === false) {
+		echo 'Connection error: ' . curl_error($ch) . "\n";
+		return null;
+	}
 
-    curl_close($ch);
+	curl_close($ch);
 
-    return $response;
+	return $response;
 }
 
 
