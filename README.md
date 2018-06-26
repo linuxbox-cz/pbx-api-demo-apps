fb2df3bc9afc77997920bd1eaf94aad52717d942
Martin Grames <mgrames@linuxbox.cz>
2018-06-26 13:14:07 +0200

    First version.

---
+++ README.md
@@ -0,0 +1,4 @@
+Demo applications for LinuxBox.cz PBX API
+=========================================
+
+* rest-api.php - PHP demo for PBX REST API
---
+++ rest-api.php
@@ -0,0 +1,34 @@
+<?php
+define('URL', 'https://custpbx.lbox.cz:8443/');
+define('CERT', './custpbx-demo1.pem');
+
+function api_call($action, $data) {
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, URL . $action);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_SSLCERT, CERT);
+
+    $response = curl_exec($ch);
+    if ($response === false) {
+        echo 'Connection error: ' . curl_error($ch) . "\n";
+        return null;
+    }
+
+    curl_close($ch);
+
+    return $response;
+}
+
+
+$res = api_call('exten_dial', array(
+	'exten' => '100',
+	'number' => '123456789'
+));
+print_r($res);
+echo "\n";
